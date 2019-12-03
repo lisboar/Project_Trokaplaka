26c1efe710f94fd7090cb42e8f60e9c3f5daa999
Thainã Lisboa <lisboagxf@gmail.com>
2019-12-03 00:36:32 -0300

    Criado a conexão com o BD local e a primeira tela de Login com um check informando se a conexão com o BD foi bem sucedida.

---
+++ src/DAO/ModuloConexao.java
@@ -0,0 +1,30 @@
+package DAO;
+
+import java.sql.*;
+
+/**
+ *
+ * @author Thainã
+ */
+public class ModuloConexao {
+    
+    public static Connection conector(){
+        
+        java.sql.Connection conexao = null;
+        
+        String driver = "com.mysql.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/dbtrokaplaka";
+        String user = "root";
+        String password = "";
+        
+        try{
+            Class.forName(driver);
+            conexao=DriverManager.getConnection(url, user, password);
+            return conexao;
+        }catch (Exception e){
+            System.out.println(e);
+            return null;
+        }
+    }
+    
+}
